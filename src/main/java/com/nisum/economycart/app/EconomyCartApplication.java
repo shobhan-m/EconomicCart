daa75dbc679e094e19570f2b4a44f0dd295d7c6d
nisum <smamidala@nisum.com>
2017-11-23 10:22:43 +0530

    first commit

---
+++ src/main/java/com/nisum/economycart/app/EconomyCartApplication.java
@@ -0,0 +1,12 @@
+package com.nisum.economycart.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconomyCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconomyCartApplication.class, args);
+	}
+}
---
+++ src/main/java/com/nisum/economycart/app/util/Constants.java
@@ -0,0 +1,26 @@
+package com.nisum.economycart.app.util;
+
+public class Constants {
+
+	public static final String MSG_RECORD_ADD = "Record Added Successfully !!";
+	public static final String MSG_RECORD_UPDATE = "Record Updated Successfully !!";
+	public static final String MSG_RECORD_NOT_UPDATE = "Sorry Record Not Updated !!";
+	public static final String QUESTION_NOT_EXIST = "Question Details doest'n Exist !!";
+	public static final String REPLY_NOT_EXISTS = "Reply Details doest'n Exist !!";
+
+	public static final String ERROR_MESSAGE = "Error While Creating Record !!";
+	public static final String UNKNOWN_EXCEPTION = "Unknown Exception !!";
+	public static final String INTERNALSERVERERROR = "Internal Server Error !!";
+	
+
+	public static final String USER_UPDATED = "User updated Successfully !!";
+	public static final String USER_DELETED = "User Deleted Successfully !!";
+	public static final String USER_NOT_EXISTS = "User doesnot exists !!";
+	public static final String USERS_NOT_AVALIABLE = "Users Not Available !!";
+
+	
+	
+	
+	
+	
+}
