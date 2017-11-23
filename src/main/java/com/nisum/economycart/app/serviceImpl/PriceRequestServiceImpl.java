867ca0547680139322eb65df8e8856d6acb5b58a
Vasu Anupoju <Mohana_Anupoju@gap.com>
2017-11-23 15:54:05 +0530

    [Vasu, Mahesh]: Price Request changes: successfully added in db

--- src/main/java/com/nisum/economycart/app/serviceImpl/PriceRequestServiceImpl.java
+++ src/main/java/com/nisum/economycart/app/serviceImpl/PriceRequestServiceImpl.java
@@ -1,28 +1,49 @@
 package com.nisum.economycart.app.serviceImpl;
 
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
+import org.apache.commons.lang3.ObjectUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Service;
 
 import com.nisum.economycart.app.domain.PriceRequest;
+import com.nisum.economycart.app.dto.ServiceStatusDto;
 import com.nisum.economycart.app.repositroy.PriceRequestRepository;
 import com.nisum.economycart.app.service.PriceRequestService;
-@Configuration
-public class PriceRequestServiceImpl implements PriceRequestService{
+import com.nisum.economycart.app.util.Constants;
 
+@Configuration
+public class PriceRequestServiceImpl implements PriceRequestService {
+	private static Logger logger = LoggerFactory.getLogger(PriceRequestServiceImpl.class);
 	@Autowired
 	PriceRequestRepository priceRequestRepository;
+
 	@Override
-	public PriceRequest addPriceRequest(PriceRequest priceRequest) {
-		LocalDateTime createDate = LocalDateTime.now();
+	public ServiceStatusDto addPriceRequest(PriceRequest priceRequest) {
+		logger.info("PriceRequestServiceImpl : addPriceRequest : "+ priceRequest.toString());
+		ServiceStatusDto status = new ServiceStatusDto();
+		LocalDateTime presentDate = LocalDateTime.now();
+		Timestamp createDate = Timestamp.valueOf(presentDate);
 		int daysRange = priceRequest.getSelectDaysRange();
-		LocalDateTime toDate = createDate.plusDays(daysRange);
+		LocalDateTime featureDate = presentDate.plusDays(daysRange);
+		Timestamp toDate = Timestamp.valueOf(featureDate);
 		priceRequest.setCreateDate(createDate);
 		priceRequest.setToDate(toDate);
 		priceRequest = priceRequestRepository.save(priceRequest);
-		return priceRequest;
+		if (ObjectUtils.anyNotNull(priceRequest)) {
+			logger.info("PriceRequestServiceImpl : addPriceRequest : Inside If Condition");
+			status.setStatus(true);
+			status.setMessage(Constants.PRICE_REQUEST_SUCCESS);
+			return status;
+		} else {
+			logger.info("PriceRequestServiceImpl : addPriceRequest : Inside else Condition");
+			status.setStatus(false);
+			status.setMessage(Constants.PRICE_REQUEST_FAILED);
+		}
+		return status;
 	}
 
 }
--- src/main/java/com/nisum/economycart/app/util/Constants.java
+++ src/main/java/com/nisum/economycart/app/util/Constants.java
@@ -11,36 +11,36 @@ public class Constants {
 	public static final String ERROR_MESSAGE = "Error While Creating Record !!";
 	public static final String UNKNOWN_EXCEPTION = "Unknown Exception !!";
 	public static final String INTERNALSERVERERROR = "Internal Server Error !!";
-	
 
 	public static final String USER_UPDATED = "User updated Successfully !!";
 	public static final String USER_DELETED = "User Deleted Successfully !!";
 	public static final String USER_NOT_EXISTS = "User doesnot exists !!";
 	public static final String USERS_NOT_AVALIABLE = "Users Not Available !!";
 
-	
 	// Email configuration for new user account
-			public static final String GMAIL_SMTP="mail.smtp.auth";
-			
-			public static final String TRUE_FLAG="true";
-			
-			public static final String GMAIL_START_TLS="mail.smtp.starttls.enable";
-			
-			public static final String  GMAIL_HOST="mail.smtp.host";
-			
-			public static final String GMAIL_PORT="mail.smtp.port";
-			
-			public static final String  GMAIL_PORT_NUM="587";
-			
-			public static final String GMAIL_HOST_NAME="smtp.gmail.com";
-			
-			public static final String SORT_BY_ELEMENT="createdDate";
-			
-			//Notifications
-			public static final String NOTIFICATION_TYPE_QUESTION="Questionaries";
-			
-			public static final String YES="Y";
-	
-	
-	
+	public static final String GMAIL_SMTP = "mail.smtp.auth";
+
+	public static final String TRUE_FLAG = "true";
+
+	public static final String GMAIL_START_TLS = "mail.smtp.starttls.enable";
+
+	public static final String GMAIL_HOST = "mail.smtp.host";
+
+	public static final String GMAIL_PORT = "mail.smtp.port";
+
+	public static final String GMAIL_PORT_NUM = "587";
+
+	public static final String GMAIL_HOST_NAME = "smtp.gmail.com";
+
+	public static final String SORT_BY_ELEMENT = "createdDate";
+
+	// Notifications
+	public static final String NOTIFICATION_TYPE_QUESTION = "Questionaries";
+
+	public static final String YES = "Y";
+
+	public static final String PRICE_REQUEST_SUCCESS = "Price Request Has Send Successfully !!";
+	public static final String PRICE_REQUEST_FAILED = "Sorry Price Request failed !!";
+	public static final String MSG_UPDATE_ECART_FAILED = "Contact Customer Care !!";
+
 }
