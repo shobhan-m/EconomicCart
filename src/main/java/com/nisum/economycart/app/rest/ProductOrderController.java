cf4b75b11e55bdceff985f39dbf346d3890d4688
manohar <mdhavala@nisum.com>
2017-11-23 12:09:34 +0530

    Added checkPriceIsLess method

---
+++ src/main/java/com/nisum/economycart/app/rest/ProductOrderController.java
@@ -0,0 +1,39 @@
+package com.nisum.economycart.app.rest;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort.Order;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nisum.economycart.app.service.ProductService;
+
+
+
+
+@RestController
+public class ProductOrderController {
+
+	private static final Logger log = LoggerFactory.getLogger(ProductOrderController.class);
+	
+	@Autowired
+	ProductService productService;
+	
+	
+	
+	@RequestMapping(value="/checkPrice", method=RequestMethod.GET)
+	public boolean checkIfPriceIsLess(float price, String productId) {
+		return productService.checkIfPriceIsLess(price, productId);
+	
+		
+	}
+		
+		
+}
