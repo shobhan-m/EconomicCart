2dceaa66204f152725eb5054d0950f4f7b84225b
manohar <mdhavala@nisum.com>
2017-11-23 21:05:15 +0530

    updated price message

--- src/main/java/com/nisum/economycart/app/service/UserService.java
+++ src/main/java/com/nisum/economycart/app/service/UserService.java
@@ -3,7 +3,7 @@ package com.nisum.economycart.app.service;
 import com.nisum.economycart.app.domain.User;
 
 public interface UserService {
-	
+	//
 	public boolean validateUser(String emailId);
 	public String sendEmail(User user,float changedprice);
 }
